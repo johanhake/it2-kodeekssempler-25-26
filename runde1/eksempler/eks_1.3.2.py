373bd3942144e6d615e9603ffe394f897c9b36b0
johanhake <johan.hake@gmail.com>
2025-08-27 10:27:40 +0200

    La til fra timen

--- runde1/eksempler/eks_1.3.2.py
+++ runde1/eksempler/eks_1.3.2.py
@@ -1,22 +1,21 @@
 tall = 13
 
 # Tallet 13 er 00001101 binært.
-print(f'Tallet {tall} er {tall} binært.')          
+print(f'Tallet {tall} er {tall:08b} binært.')          
 # fill=0, width=8 og type=b
 
 # Tallet 13 er 0d i heksadesimalt.
-print(f'Tallet {tall} er {tall} heksadesimalt.') 
+print(f'Tallet {tall} er {tall:x} heksadesimalt.') 
 # fill=0, width=2 og type=x
-
 verdier =[1.1, 22.22, 333.333]
 #      1.10
 #     22.22
 #    333.33
 for verdi in verdier:
-    print(f'{verdi}') 
+    print(f'{verdi:10.2f}') 
 # align=>, width=10 og type=f, precision=2 og type=f
 
 tall = 1/3
-print(tall) # Bruk round() 0.33
-print(f'{tall}') # 0.333
-print(f'{tall}') # 3.3e-01 (standard form)
+print(round(tall, 2)) # Bruk round() 0.33
+print(f'{tall:.2f}') # 0.333
+print(f'{tall:.1e}') # 3.3e-01 (standard form)
