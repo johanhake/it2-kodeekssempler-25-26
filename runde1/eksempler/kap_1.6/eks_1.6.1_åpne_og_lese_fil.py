e1a143580ff17e8e290e48f1ac61d29f27f92fc8
johanhake <johan.hake@gmail.com>
2025-09-08 14:35:55 +0200

    La til eksempler i timen

--- runde1/eksempler/kap_1.6/eks_1.6.1_åpne_og_lese_fil.py
+++ runde1/eksempler/kap_1.6/eks_1.6.1_åpne_og_lese_fil.py
@@ -1,29 +1,31 @@
 with open('jada.txt') as fil:
     # jada\nJohan
-    data = fil
+    data = fil.read()
     print('read():')
     print(data)
 
     # ada
-    fil.seek(0)
+    fil.seek(1)
     data = fil.read(3)
     print('\nseek(); read():')
     print(data)
 
     # ["jada\n", "Johan\n"]
-    #fil.seek(0)
+    fil.seek(0)
     linjer = fil.readlines()
     print('\nreadlines():')
     print(linjer)
 
-    # jada,johan
+    # jada
+    # johan
     fil.seek(0)
     print('\nreadline():')    
-    print(fil.readline())
+    print(fil.readline(), end="")
     print(fil.readline())
 
     # JADA,JOHAN,
     fil.seek(0)
     print('\nfor linje in fil:')
     for linje in fil:
-        print(linje)
+        # .strip(), Fjerner alle tomrom på start og slutt av string. 
+        print(linje.strip(), end=",")
--- runde1/eksempler/kap_1.6/eks_1.6.2_skrive_til_fil.py
+++ runde1/eksempler/kap_1.6/eks_1.6.2_skrive_til_fil.py
@@ -2,7 +2,7 @@
 # 2: TO
 # 3: TRE
 # æ ø å Æ Ø Å
-with open('norsk.txt', mode='w', encoding="ascii") as fil:
+with open('norsk.txt', mode='w', encoding="utf-8") as fil:
     fil.write('1: EN\n2: TO\n3: TRE\n')
     fil.write('æ ø å Æ Ø Å')
 
