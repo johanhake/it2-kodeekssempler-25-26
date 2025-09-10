559a6e95be2b05136dffbf77100cbc8fcf060f7d
johanhake <johan.hake@gmail.com>
2025-09-10 09:07:19 +0200

    liten endring

--- runde1/eksempler/kap_1.7/eks_1.7.4_assert.py
+++ runde1/eksempler/kap_1.7/eks_1.7.4_assert.py
@@ -5,9 +5,3 @@ def aldersgruppe(alder):
         return 'voksen'
 
 assert aldersgruppe(10)=='barn'
-assert aldersgruppe(17)=='barn'
-assert aldersgruppe(18)=='voksen'
-assert aldersgruppe(50)=='voksen'
-assert aldersgruppe(66)=='voksen'
-assert aldersgruppe(67)=='pensjonist'
-assert aldersgruppe(80)=='pensjonist'
