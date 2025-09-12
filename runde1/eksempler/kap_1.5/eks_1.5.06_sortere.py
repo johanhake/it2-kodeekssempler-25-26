2ccd545e9c2059d1961e9bbf6a61b2e7ae5cadaa
johanhake <johan.hake@gmail.com>
2025-09-12 11:02:19 +0200

    La til sorterings eksempel

--- runde1/eksempler/kap_1.5/eks_1.5.06_sortere.py
+++ runde1/eksempler/kap_1.5/eks_1.5.06_sortere.py
@@ -1,19 +1,38 @@
-print('sort:')
-venner = ['Ole','Dole','Doffen']
+print("sort:")
+venner = ["Ole", "Dole", "Doffen"]
 print(venner)
 
 # ['Doffen', 'Dole', 'Ole']
 venner.sort()
 print(venner)
 
-print('sorted:')
-venner = ['Ole','Dole','Doffen']
+print("sorted:")
+venner = ["Ole           ", "Dole", "Doffen"]
 print(sorted(venner))
 # ['Doffen', 'Dole', 'Ole']
 print(venner)
 
-print('reversed:')
+print("reversed:")
 print(venner)
 # ['Doffen', 'Dole', 'Ole']
 venner.reverse()
 print(venner)
+
+
+def lengde(s):
+    return len(s)
+
+
+venner.sort(key=lengde)
+print()
+print(venner)
+
+def alder(l):
+    return l[1]
+
+lærere = [["Johan", 49], 
+          ["Jarl", 57],
+          ["Geir", 54], 
+          ["Joar", 53]]
+
+print(sorted(lærere, key=alder, reverse=True))
