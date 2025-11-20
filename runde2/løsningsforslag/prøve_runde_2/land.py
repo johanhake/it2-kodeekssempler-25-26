871cdc4c9abbaf12bdc23041193992a882ce6ce6
johanhake <johan.hake@gmail.com>
2025-11-20 13:53:00 +0100

    La til kommentarer

--- runde2/løsningsforslag/prøve_runde_2/land.py
+++ runde2/løsningsforslag/prøve_runde_2/land.py
@@ -37,8 +37,14 @@ print(f"Det er {len(land_liste)} antall land i filen")
 
 innbyggere = 0
 kontinenter = {}
+
+# Lager en ordbok med kontinentene som nøkler og alle land-ordbøkene i lister som verdi.
 for land in land_liste:
+    
+    # Adderer alle inbyggene i alle landene
     innbyggere += land["innbyggere"]
+
+    # Er kontinenten til landet i ordboken?
     if land["kontinent"] in kontinenter:
         kontinenter[land["kontinent"]].append(land)
     else:
@@ -47,18 +53,23 @@ for land in land_liste:
 print(f"Antall inbyggere i alle land: {formater_innbyggere(innbyggere)}")
 print(f"Antall kontinenter: {len(kontinenter.keys())}")
 
+# Finner lengden til listene for hvert kontinent. Det representerer antall kontinent
 def antall_land(kont):
     return len(kont[1])
 
+# Sorterer alle kontinentene etter antall land som nøkkel
 sorterte_kontinenter = sorted(kontinenter.items(), reverse=True, key=antall_land)
 
+# Viser de tre første landene
 print("\nKontinenter med flest land:")
 for kontinent, land_liste in sorterte_kontinenter[:3]:
     print(f"{kontinent} har {len(land_liste)} antall land")
 
 print()
+#  Går igjennom alle kontinentene med listene med land-ordbøker
 for kontinent, land_liste in sorterte_kontinenter:
     innbyggere = 0
+    # Adderer innbyggene for hvert kontinent og skriver ur det. 
     for land in land_liste:
         innbyggere += land["innbyggere"]
     print(f"{kontinent} har {formater_innbyggere(innbyggere)} innbyggere")    
