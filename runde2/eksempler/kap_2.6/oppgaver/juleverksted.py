bb976ba919a83b764f82698669166d5dbf3c14cf
johanhake <johan.hake@gmail.com>
2025-12-07 16:37:54 +0100

    La inn endringer fra timen

--- runde2/eksempler/kap_2.6/oppgaver/juleverksted.py
+++ runde2/eksempler/kap_2.6/oppgaver/juleverksted.py
@@ -12,18 +12,29 @@ class Nisse:
         raise NotImplementedError()
     
 class Nissefar(Nisse):
-    def __init__(self) -> None:
-        ???
+    def __init__(self, alder) -> None:
+        super().__init__("Nissefar", alder)
+        self.gaver = []
 
     def utfør(self, antall: int) -> list:
-        ???
+        if len(self.gaver) < antall:
+            print("Ikke nok gaver!")
+        else:
+            # List comprehension
+            # return [self.gaver.pop() for i in range(antall)]
+            ret = []
+            for i in range(antall):
+                ret.append(self.gaver.pop())
+            
+            return ret
+            
     
     def info(self) -> str:
         return f"{self.navn} er {self.alder} år og han deler ut gaver."
 
 class Nissemor(Nisse):
-    def __init__(self) -> None:
-        ???
+    def __init__(self, alder) -> None:
+        super().__init__("Nissemor", alder)
 
     def utfør(self) -> str:
         return "Grøt!!"
@@ -35,41 +46,49 @@ class Arbeidsnisse(Nisse):
     # Klass-attributt. Et attributt som er lik for ALLE objekter. Da lagres det i klassen
     mulige_gaver = ["politibil", "lekebil", "dukke", "bamse", "leketåg", "sykkel", "servise"]
 
-    def __init__(self, navn: str, alder: int) -> None:
-        ???
-    
     # Metode som er lik for alle objekter i en klasse. 
     # Kan bruke en klassemetode (men det tar vi i runde 3)
     def utfør(self) -> str:
-        return ???
+        return choice(["politibil", "lekebil", "dukke", "bamse", "leketåg", "sykkel", "servise"])
 
     def info(self) -> str:
         return f"{self.navn} er arbeidsnisse og er {self.alder} år."
     
 class Juleverksted:
     def __init__(self, lokasjon) -> None:
-        self.lokasjon = ???
-        self.gaver = ???
-        self.arbeidsnisser = ???
-        self.far = ???
-        self.mor = ???
+        self.lokasjon = "Nordpolen"
+        self.gaver = []
+        self.arbeidsnisser = []
+        self.far = Nissefar(150)
+        self.mor = Nissemor(145)
         
     def legg_til_arbeidsnisse(self, nisse:Arbeidsnisse) -> None:
         self.arbeidsnisser.append(nisse)
 
     def produser(self) -> None:
         for nisse in self.arbeidsnisser:
-            ???
+            self.gaver(nisse.utfør())
 
     def vis_nisser(self) -> None:
         print(self.far.info())
         print(self.mor.info())
         for nisse in self.arbeidsnisser:
-            ???
+            print(nisse.info())
 
     def overfør_gaver_til_nissefar(self):
-        self.far.gaver ???
-        self.gaver = ???
+        
+        self.far.gaver = self.gaver
+        self.gaver.clear()
+        
+        
+        self.far.gaver = self.gaver.copy()
+        
+        self.far.gaver.extend(self.gaver)
+        
+        for gave in self.gaver:
+            self.far.gaver.append(gave)
+        
+        self.gaver.clear()
 
 if __name__ == "__main__":
     verksted = Juleverksted("Norpolen")
