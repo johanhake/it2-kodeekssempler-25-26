19853aa4400937518e3417a4edc469b9f60e303e
johanhake <johan.hake@gmail.com>
2025-12-19 16:18:21 +0100

    La til kode for rektangler

--- runde3/eksempler/kap3.1/rektangler.py
+++ runde3/eksempler/kap3.1/rektangler.py
@@ -3,17 +3,17 @@ import numpy as np
 # leser inn fil med data og lager 2D array med heltall
 data = np.loadtxt("rektangler.txt", delimiter=",", dtype=int)
 
-#print(data)
+print(data)
 
 # Henter ut første og andre kolonnen
 bredder = data[:,0]
 lengder = data[:,1]
 
-#print(bredder, lengder)
+print(bredder, lengder)
 
-areal = bredder*lengder
-print(areal)
+# areal = bredder*lengder
+# print(areal)
 
-print("a)", bredder[1::2]*lengder[1::2])
-print("b)", bredder[1::2]*lengder[::2])
-print("c)", bredder*lengder[::-1])
+# print("a)", bredder[1::2]*lengder[1::2])
+# print("b)", bredder[1::2]*lengder[::2])
+# print("c)", bredder*lengder[::-1])
