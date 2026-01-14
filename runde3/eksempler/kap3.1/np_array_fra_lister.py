686f4b7018027add188ed964ccffe6ad788b1246
johanhake <johan.hake@gmail.com>
2026-01-14 11:19:00 +0100

    Siste løsningsforslagene til grafoppg

--- runde3/eksempler/kap3.1/np_array_fra_lister.py
+++ runde3/eksempler/kap3.1/np_array_fra_lister.py
@@ -5,21 +5,32 @@ tall1 = np.array([-1, 1, 3, 5, 9])
 tall2 = np.array([-2, 2, 6, 10, 14])
 
 # Verdi tabell zip og enumerate
-for i, (t1, t2) in enumerate(zip(tall1, tall2)):
-    print(f"{i+1} | {t1:2} | {t2:2}")
+# for i, (t1, t2) in enumerate(zip(tall1, tall2)):
+#     print(f"{i+1} | {t1:2} | {t2:2}")
 
-print(tall1[2:])
-print(tall2[:3])
-print(tall1[::2])
-print(tall1[1:]-tall1[:-1])
+# def f(x):
+#     return x**2 -2*x + 5
 
-# Viser grafen til den deriverte til y
-x = tall1
-y = tall2
-print((y[1:]-y[:-1])/(x[1:]-x[:-1]))
-plt.plot(x, y)
+# x = np.linspace(-5,5, 11)
+# print(f(x))
+# plt.plot(x,f(x))
+# plt.show()
+
+# print(tall1[2:])
+# print(tall2[:3])
+# print(tall1[::2])
+# print(tall1[1:]-tall1[:-1])
+
+# # Viser grafen en funksjon 
+# x = tall1
+# y = tall2
+# print((y[1:]-y[:-1])/(x[1:]-x[:-1]))
+# plt.plot(x, y)
+# plt.show()
 
 # Bruker boolske arrays til å hente ut verdier fra numpy array
+x = tall1
+y = tall2
 print(x)
 print(y)
 ind = np.array([True, False, True, True, False])
@@ -29,4 +40,3 @@ print(y[ind])
 print(x > 2)
 print(y[x > 2])
 
-plt.show()
