b42c759eeb2e4b9d66147159a32780b104583fc8
johanhake <johan.hake@gmail.com>
2026-01-28 10:27:32 +0100

    La til mustegning

--- runde3/eksempler/kap3.4/eks_3.4.4-tegne-med-mus.py
+++ runde3/eksempler/kap3.4/eks_3.4.4-tegne-med-mus.py
@@ -14,17 +14,22 @@ while fortsett:
             fortsett = False
 
         # Check for mouse events
-        if event.type == pg.MOUSEBUTTONDOWN:
-            print(f"Mouse button pressed at {event.pos}, button: {event.button}")
+        # if event.type == pg.MOUSEBUTTONDOWN:
+        #     print(f"Mouse button pressed at {event.pos}, button: {event.button}")
+        #     pg.draw.circle(vindu, "red", event.pos, 10)
         
-        if event.type == pg.MOUSEBUTTONUP:
-            print(f"Mouse button released at {event.pos}, button: {event.button}")
+        # if event.type == pg.MOUSEBUTTONUP:
+        #     print(f"Mouse button released at {event.pos}, button: {event.button}")
             
         if event.type == pg.MOUSEMOTION:
             # event.pos gives absolute position (x, y)
             # event.rel gives relative movement (rel_x, rel_y)
             # event.buttons is a tuple of boolean states for (left, middle, right) buttons
             pass # Use this for tracking mouse movement
+            print(f"Mouse move event {event.pos}, button: {event.buttons}")
+            if event.buttons[0]:
+                pg.draw.circle(vindu, "red", event.pos, 10)
+            
 
     pg.display.update()
 
