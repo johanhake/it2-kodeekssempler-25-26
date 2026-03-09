8ece1ddc9e3e49c0eda217020def588be91c6761
johanhake <johan.hake@gmail.com>
2026-03-09 15:34:19 +0100

    La til løsningsforslag til 4.2.1 og 4.2.2

---
+++ runde4/løsningsforslag/kap4.2/oppg_4.2.2_ball_mot_ball.py
@@ -0,0 +1,78 @@
+# Kollisjon mellom to baller
+import pygame as pg
+from pygame.locals import *
+from random import randint
+
+WIDTH, HEIGHT = 400, 100
+FPS = 24
+
+
+class Ball(pg.sprite.Sprite):
+    def __init__(self, app, color, fart, pos="left"):
+        super().__init__()
+        self.image = pg.Surface((50, 50), pg.SRCALPHA)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = 0 if pos == "left" else WIDTH - self.rect.width
+        self.rect.y = (HEIGHT - self.rect.height) / 2
+        pg.draw.circle(self.image, color, (25, 25), 25)
+        pg.draw.circle(self.image, "yellow", (25, 25), 25, 5)
+        self.radius = 25
+        self.vx = fart
+        self.app = app
+        self.app.all_sprites.add(self)
+
+    def update(self):
+        self.rect.x += self.vx
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.vx *= -1
+            self.rect.x += self.vx        
+            
+    def collide(self, andre):
+        self.vx, andre.vx = andre.vx, self.vx
+
+        self.rect.x += self.vx
+        andre.rect.x += andre.vx
+
+class App:
+    def __init__(self):
+        pg.init()
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Baller som kolliderer")
+        self.running = True
+        self.all_sprites = pg.sprite.Group()
+        self.ball1 = Ball(self, "red", 5, "left")
+        self.ball2 = Ball(self, "blue", -2, "right")
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+
+    def update(self):
+        if pg.sprite.collide_circle(self.ball1, self.ball2):
+            self.ball1.collide(self.ball2)
+            
+        # Fortsett her
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill("black")
+        self.all_sprites.draw(self.screen)
+        pg.display.update()
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+        pg.quit()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
+
+# Smidig IT-2 © TIP AS, 2024
